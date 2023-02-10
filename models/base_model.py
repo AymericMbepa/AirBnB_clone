8c0d71ea0c6547ee554b9e17d99999dc403cd78a
AymericMbepa <aymericmbepa@gmail.com>
2023-02-09 19:40:30 -0800

    Base model

---
+++ models/base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import uuid
+import datetime
+
+
+class BaseModel:
+    """
+    define all common attribute methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        init method for BaseModel class
+
+        Attribute:
+           id: string- assign with an uuid when an instance is created
+           create_at: date_time- assign with the current date_time when
+           an instance
+                   is created
+           Updated_at: date_time- assign whit the current date_time when an
+                    instance is created and it will be updated everytime you
+                    change your oblect
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(value,
+                                                       '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+
+    def __str__(self):
+        """
+        str method for BaseModel class
+
+        """
+        return str(([self.__class__.__name__], (self.id), (self.__dict__)))
+
+    def save(self):
+        """
+        update the publis instance attribute update_at with the current
+        datetime
+
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+         returns a dictionary containing all keys/values of __dict__ of
+        the instance
+
+        """
+        return {**self.__dict__, 'created_at': self.created_at.isoformat(),
+                'updated_at': self.updated_at.isoformat()}
